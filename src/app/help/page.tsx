fdf3a658f10f1a13e2fa0151a2059cce8b49eab6
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2024-06-22 00:18:14 +0100

    added start modal

---
+++ src/app/help/page.tsx
@@ -0,0 +1,7 @@
+export default function Help(){
+  return(
+  <main>
+      todo help page
+  </main>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client"
+import StartModal from "@/components/modal/modal";
 import ToolBar from "@/components/toolBar/ToolBar";
 import { LatLngEditor } from "@/components/waypointEditor/LatLngEditor";
 import WaypointEditor from "@/components/waypointEditor/WaypointEditor";
-import CurEdit from "@/components/waypointEditor/curEdit";
 import HeightMap from "@/components/waypointEditor/heightMap";
 import ListView from "@/components/waypointList/ListView";
 import dynamic from "next/dynamic";
@@ -15,6 +15,7 @@ const NonSSRMap = dynamic(
 export default function Home() {
   return (
     <main style={{height: "100vh"}} className="flex flex-col absolute w-full">
+      <StartModal/>
       <ToolBar/>
       <div className="grid grid-cols-[1fr_300px] grid-rows-[1fr_300px] h-full">
         <div className="row-start-1 col-start-1">
