b31f460e62bbd727a1090b6fb5ef428cf83adf5e
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2024-06-28 18:35:44 +0100

    moved to react compiler + fixed terrain data presicion

--- src/util/terrain.ts
+++ src/util/terrain.ts
@@ -1,15 +1,23 @@
 type openElevation = {elevation: number, latitude: number, longitude: number}[]
 export function getTerrain(locs: [number, number][]): Promise<openElevation|null>{
-  if (locs.length == 0) return new Promise(()=>null)
-  let locstring = ""
-  for (let i = 0; i < locs.length; i++){
-    locstring += `${locs[i][0]},${locs[i][1]}|`
-  }
+  if (locs.length == 0) return Promise.resolve(null)
+  let locstring = locs.map((loc) => `${loc[0].toFixed(7)},${loc[1].toFixed(7)}`).join("|")
+
   return fetch(`https://api.open-elevation.com/api/v1/lookup?locations=${locstring}`)
-    .then((x)=>x.json())
-    .then((x)=>x.results as openElevation)
-    .catch((error)=>{
-      console.error("An error occurred while fetching terrain data:", error);
-      return null;
+    .then((response)=>{
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
     })
+    .then(data => {
+      if (!data.results) {
+        throw new Error("No results found in the response");
+      }
+      return data.results as openElevation;
+    })
+    .catch((error) => {
+      console.log("Error fetching elevation data:", error);
+      return null;
+    });
 }
