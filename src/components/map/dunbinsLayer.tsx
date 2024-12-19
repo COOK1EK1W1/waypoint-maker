155cec99d7696e3c935aa65eac5039f994985755
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2024-12-19 12:32:49 +0000

    upgraded next

--- src/components/map/dunbinsLayer.tsx
+++ src/components/map/dunbinsLayer.tsx
@@ -27,15 +27,15 @@ export default function DubinsLayer(){
   let a = activeWPs[i]
   let b = activeWPs[i+1]
   let curves = DubinsBetween({x: a.param6, y: a.param5}, {x: b.param6, y: b.param5}, a.param2 / 180 * Math.PI, b.param2 / 180 * Math.PI, 400)
-  curves.map((c) =>{
+  curves.map((c, a) =>{
     switch (c.type){
       case "Curve":
         let rWaypoint: Waypoint = {frame: 0, type: 189, param1: 0, param2: 0, param3: 0, param4: 0, param6: c.center.x, param5: c.center.y, param7: 0, autocontinue: 0}
-        markers.push(<DraggableMarker waypoint={rWaypoint} active={false}/>)
-        lines.push(<Arc curve={c}/>)
+        markers.push(<DraggableMarker key={""+i+a} waypoint={rWaypoint} active={false}/>)
+        lines.push(<Arc key={""+i+a} curve={c}/>)
         break;
       case "Straight":
-        lines.push(<Polyline pathOptions={limeOptions} positions={[{lat: c.start.y, lng: c.start.x}, {lat: c.end.y, lng: c.end.x}]} />)
+        lines.push(<Polyline key={""+i+a} pathOptions={limeOptions} positions={[{lat: c.start.y, lng: c.start.x}, {lat: c.end.y, lng: c.end.x}]} />)
         break
     }
 
