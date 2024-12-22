f78690c80a05d60072b9f0c98dd3ef0c43f58c82
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2024-12-22 01:16:37 +0000

    added particle swarm algorithm

--- src/components/map/dunbinsLayer.tsx
+++ src/components/map/dunbinsLayer.tsx
@@ -6,6 +6,8 @@ import { DubinsBetween } from "@/lib/dubins/dubins";
 import {Waypoint} from "@/types/waypoints"
 import Arc from "../marker/arc";
 import { ReactNode } from "react";
+import { pathLength } from "@/lib/dubins/geometry";
+import { particleSwarmOptimise } from "@/lib/optimisation/particleSwarm";
 
 const limeOptions = { color: 'red' }
 const noshow = ["Markers", "Geofence"]
@@ -22,24 +24,44 @@ export default function DubinsLayer(){
   let markers: ReactNode[] = []
   let lines: ReactNode[] = []
 
+  function testing(dirs: number[]): number{
+    let totalLength = 0
+
+    for (let i = 0; i < activeWPs.length - 1; i++){
+      let a = activeWPs[i]
+      let b = activeWPs[i+1]
+      let h1 = dirs[i]
+      let h2 = dirs[i+1]
+      let curves = DubinsBetween({x: a.param6, y: a.param5}, {x: b.param6, y: b.param5}, h1, h2, b.param3)
+      totalLength += pathLength(curves)
+    }
+    return totalLength
+  }
+  let start_dirs = []
+  for (let u = 0; u < activeWPs.length; u++){
+    start_dirs.push(0)
+  }
+  let optimised_dirs = particleSwarmOptimise(start_dirs, [], testing, 200)
 
   for (let i = 0; i < activeWPs.length - 1; i ++){
-  let a = activeWPs[i]
-  let b = activeWPs[i+1]
-  let curves = DubinsBetween({x: a.param6, y: a.param5}, {x: b.param6, y: b.param5}, a.param2 / 180 * Math.PI, b.param2 / 180 * Math.PI, b.param3)
-  curves.map((c, a) =>{
-    switch (c.type){
-      case "Curve":
-        let rWaypoint: Waypoint = {frame: 0, type: 189, param1: 0, param2: 0, param3: 0, param4: 0, param6: c.center.x, param5: c.center.y, param7: 0, autocontinue: 0}
-        markers.push(<DraggableMarker key={""+i+a} waypoint={rWaypoint} active={false}/>)
-        lines.push(<Arc key={""+i+a} curve={c} pathOptions={limeOptions}/>)
-        break;
-      case "Straight":
-        lines.push(<Polyline key={""+i+a} pathOptions={limeOptions} positions={[{lat: c.start.y, lng: c.start.x}, {lat: c.end.y, lng: c.end.x}]} />)
-        break
-    }
+    let a = activeWPs[i]
+    let b = activeWPs[i+1]
+    let h1 = optimised_dirs[i]
+    let h2 = optimised_dirs[i+1]
+    let curves = DubinsBetween({x: a.param6, y: a.param5}, {x: b.param6, y: b.param5}, h1, h2, b.param3)
+    curves.map((c, a) =>{
+      switch (c.type){
+        case "Curve":
+          let rWaypoint: Waypoint = {frame: 0, type: 189, param1: 0, param2: 0, param3: 0, param4: 0, param6: c.center.x, param5: c.center.y, param7: 0, autocontinue: 0}
+          markers.push(<DraggableMarker key={""+i+a} waypoint={rWaypoint} active={false}/>)
+          lines.push(<Arc key={""+i+a} curve={c} pathOptions={limeOptions}/>)
+          break;
+        case "Straight":
+          lines.push(<Polyline key={""+i+a} pathOptions={limeOptions} positions={[{lat: c.start.y, lng: c.start.x}, {lat: c.end.y, lng: c.end.x}]} />)
+          break
+      }
 
-  })
+    })
 
 
   }
---
+++ src/lib/optimisation/particleSwarm.ts
@@ -0,0 +1,65 @@
+type bound = {
+  min?: number,
+  max?: number,
+  circular?: boolean
+}
+
+export function particleSwarmOptimise(initialGuess: number[], bounds: bound[], fn: (a: number[])=>number, popsize: number): number[]{
+  let population = []
+  let velocities = []
+  let local_best_position = []
+  let local_best_value = []
+  let global_best_position = initialGuess
+  let global_best_value = fn(initialGuess)
+  for (let i = 0; i < popsize; i++){
+    let particle_pos = []
+    let particle_vel = []
+    let particle_best = []
+    for (let j = 0; j < initialGuess.length; j++){
+      let r = Math.random() * 0.1 - 0.05
+      particle_pos.push(initialGuess[j] + r)
+      particle_vel.push(Math.random() - 0.5)
+      particle_best.push(initialGuess[j] + r)
+    }
+    population.push(particle_pos)
+    velocities.push(particle_vel)
+    local_best_position.push(particle_best)
+    local_best_value.push(fn(particle_best))
+  }
+
+  for (let i = 0; i < 100; i++){
+    // update global and local best positions and values
+    for (let p = 0; p < popsize; p++){
+      let current_fitness = fn(population[p])
+      if (current_fitness < local_best_value[p]){
+        local_best_value[p] = current_fitness
+        local_best_position[p]= [...population[p]]
+      }
+      if (current_fitness < global_best_value){
+        global_best_value = current_fitness
+        global_best_position = [...population[p]]
+      }
+    }
+
+
+    for (let p = 0; p < popsize; p++){
+
+      for (let a = 0; a < initialGuess.length; a++){
+        let b = Math.random() * 0.5 //beta cognitive
+        let d = Math.random() * 0.5 //delta global
+
+        let newVel = 0.8 * velocities[p][a] +
+                     b * (local_best_position[p][a] - population[p][a]) + 
+                     d * (global_best_position[a] - population[p][a])
+        velocities[p][a] = newVel
+        population[p][a] += 1 * newVel
+      }
+    }
+
+  }
+  return global_best_position
+
+}
+
+
+
