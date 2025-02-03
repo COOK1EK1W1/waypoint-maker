06aa8a331b94e3025b32baddc74a40299e6ef1be
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2025-02-03 16:22:53 +0000

    make sure users are authorised

--- src/app/m/[missionid]/page.tsx
+++ src/app/m/[missionid]/page.tsx
@@ -1,14 +1,19 @@
 import { WMEditor } from "@/components/editor";
 import ToolBar from "@/components/toolBar/ToolBar";
+import { auth } from "@/util/auth";
 import CloudWaypointProvider from "@/util/context/cloudWaypointProvider";
 import VehicleProvider from "@/util/context/VehicleTypeProvider";
 import prisma from "@/util/prisma";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function Mission({ params }: { params: Promise<{ missionid: string }> }) {
   const missionId = (await params).missionid
-  const a = await prisma.mission.findUnique({ where: { id: missionId } })
+
+  let data = await auth.api.getSession({ headers: await headers() })
+  const a = await prisma.mission.findUnique({ where: { id: missionId, userId: data?.user.id } })
   if (a == null) {
-    return null
+    return redirect("/")
   }
 
   let b = JSON.parse(a.data)
