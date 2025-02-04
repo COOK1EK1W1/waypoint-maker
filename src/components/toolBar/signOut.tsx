e29df2ec45ccef963d3470f09ce5e151001addb4
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2025-02-04 09:58:10 +0000

    changed signin to modal

--- src/components/toolBar/signOut.tsx
+++ src/components/toolBar/signOut.tsx
@@ -2,8 +2,11 @@
 
 import { authClient } from "@/util/auth-client"
 import Button from "./button"
+import { useRouter } from "next/navigation"
 
 export default function SignOut() {
+  const router = useRouter()
   const { signOut } = authClient
-  return (<Button onClick={() => signOut()}>Sign Out</Button>)
+  return (<Button onClick={() => { signOut(); router.refresh() }
+  }> Sign Out</Button >)
 }
