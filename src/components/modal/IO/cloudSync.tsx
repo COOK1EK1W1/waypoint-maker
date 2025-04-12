7dee2f5f7dd79037325b7444e01bb8d63ece2c60
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2025-04-12 14:05:42 +0100

    add sync now for base url mission

--- src/components/modal/IO/cloudSync.tsx
+++ src/components/modal/IO/cloudSync.tsx
@@ -1,33 +1,58 @@
 "use client"
 import Button from "@/components/toolBar/button";
 import { useWaypoints } from "@/util/context/WaypointContext";
-import { syncMission } from "./syncAction";
+import { createNewMission, syncMission } from "./syncAction";
 import { useState, useTransition } from "react";
 import { FaCheck, FaSpinner } from "react-icons/fa";
 import { exportwpm2 } from "@/lib/missionIO/wm2/spec";
 import { useVehicle } from "@/util/context/VehicleTypeContext";
+import { useSession } from "@/util/auth-client";
 
 export default function CloudSync() {
+  const { data } = useSession()
+
   const { waypoints, missionId } = useWaypoints();
   const { vehicle } = useVehicle();
 
   const [isPending, startTransition] = useTransition();
   const [b, setb] = useState(false)
-  if (missionId) {
-    return (
-      <div className="flex items-center">
-        <Button onClick={() => startTransition(() => syncMission(missionId, exportwpm2(waypoints, vehicle)).then(() => { setb(true) }))}> Sync Now</Button>
-        <span className="pl-1">
-          {isPending ? <FaSpinner className="animate-spin" /> : b ? <FaCheck /> : null}
-        </span>
-      </div >
 
-    )
-  } else {
-    // todo convert to cloud synced
-    return <div>Not Synced</div>
-
+  // don't show anything if user isn't logged in
+  if (data?.user == undefined) {
+    return <div>Please sign in to use cloud syncing</div>
   }
+
+  // on base url create a new mission in DB
+  const handleCreateNew = () => {
+    const title = prompt("Enter Title")
+    if (title === null || title.length === 0) {
+      return null
+    }
+    startTransition(() => {
+      createNewMission(title, exportwpm2(waypoints, vehicle))
+    })
+  }
+
+  // base url, convert to stored mission
+  if (missionId === undefined) {
+    return <Button onClick={handleCreateNew}>Sync Now</Button>
+  }
+
+  const handleSync = () => {
+    startTransition(() => {
+      syncMission(missionId, exportwpm2(waypoints, vehicle)).then(() => { setb(true) })
+    })
+  }
+
+  return (
+    <div className="flex items-center">
+      <Button onClick={handleSync}> Sync Now</Button>
+      <span className="pl-1">
+        {isPending ? <FaSpinner className="animate-spin" /> : b ? <FaCheck /> : null}
+      </span>
+    </div >
+
+  )
 
 }
 
--- src/components/modal/IO/syncAction.ts
+++ src/components/modal/IO/syncAction.ts
@@ -3,10 +3,28 @@
 import { auth } from "@/util/auth"
 import prisma from "@/util/prisma"
 import { headers } from "next/headers"
+import { redirect } from "next/navigation"
 
 export async function syncMission(id: string, data: string) {
   let userData = await auth.api.getSession({ headers: await headers() })
   const userID = userData?.user.id
   if (!userID) return
   await prisma.mission.update({ where: { id: id, userId: userID }, data: { data: data, modifiedAt: new Date() } })
+}
+
+export async function createNewMission(title: string, data: string) {
+  let userData = await auth.api.getSession({ headers: await headers() })
+  const userID = userData?.user.id
+  if (!userID) return
+
+  const newMission = await prisma.mission.create({
+    data: {
+      title: title,
+      data: data,
+      public: false,
+      userId: userID
+    }
+  })
+
+  redirect(`/m/${newMission.id}`)
 }
