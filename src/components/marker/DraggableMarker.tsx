bbf850bc3cc75e03fe53f2b586bde3d40013c6f7
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2025-04-15 23:07:12 +0000

    selected icons now show when selected

--- src/components/marker/DraggableMarker.tsx
+++ src/components/marker/DraggableMarker.tsx
@@ -1,7 +1,7 @@
 import { useMemo, useRef } from "react"
 import { Marker } from "react-leaflet"
 import * as Leaflet from "leaflet"
-import { activeIcon, normalIcon } from "./waypoint"
+import { circleOverlayIcon, createAnimatedIcon } from "./waypoint"
 import { LatLng } from "@/lib/world/types"
 
 export default function DraggableMarker({ position, active, onMove, onClick }: { position: LatLng, active: boolean, onMove?: (lat: number, lng: number) => void, onClick?: () => void }) {
@@ -27,14 +27,21 @@ export default function DraggableMarker({ position, active, onMove, onClick }: {
   )
 
   return (
-    <Marker
-      draggable={true}
-      eventHandlers={eventHandlers}
-      position={position}
-      ref={markerRef}
-      icon={active ? activeIcon : normalIcon}
-    >
-      {/*<Tooltip>{waypoint.type}</Tooltip>*/}
-    </Marker>
+    <>
+      <Marker
+        draggable={true}
+        eventHandlers={eventHandlers}
+        position={position}
+        ref={markerRef}
+        icon={createAnimatedIcon(active)}
+      />
+      {active ? <Marker
+        position={position}
+        interactive={false}
+        icon={circleOverlayIcon}
+        zIndexOffset={-1000}
+      />
+        : null}
+    </>
   )
 }
