4b764a29dd38a803a90b6a37d300e28e03c7fe3f
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2025-05-10 23:12:26 +0000

    add basic kebab menu

--- src/components/dashboard/mission.tsx
+++ src/components/dashboard/mission.tsx
@@ -1,11 +1,12 @@
 "use client"
 import { useRouter } from "next/navigation";
-import { FaTrash } from "react-icons/fa";
 import Button from "../toolBar/button";
 import { deleteMission } from "./actions";
 import { timeAgo } from "@/util/time";
 import { cn } from "@/lib/utils";
 import { useWaypoints } from "@/util/context/WaypointContext";
+import { Copy, EllipsisVertical, Eye, Pencil, Share, Trash } from "lucide-react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
 
 export default function MissionTile({ mission }: { mission: { title: string, modifiedAt: Date, id: string } }) {
   const { missionId } = useWaypoints()
@@ -15,15 +16,42 @@ export default function MissionTile({ mission }: { mission: { title: string, mod
   }
 
   return (
-    <Button onClick={() => { router.push(`/m/${mission.id}`) }} className={cn("w-full px-2 bg-white p-2 m-2 flex-row", missionId == mission.id ? "bg-slate-100" : "")}>
-      <div className="flex w-full flex-col items-start">
+    <Button className={cn("w-full px-2 bg-white p-2 m-2 flex-row", missionId == mission.id ? "bg-slate-100" : "")}>
+      <div onClick={() => { router.push(`/m/${mission.id}`) }} className="flex w-full flex-col items-start">
         <p>{mission.title}</p>
 
         <p className="text-muted-foreground">Last Modified: {timeAgo(mission.modifiedAt)}</p>
       </div>
-      <div onMouseDown={(e) => { e.stopPropagation(); e.preventDefault(); handleDelete() }}>
-        <FaTrash />
-      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <div>
+            <EllipsisVertical />
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent >
+          <DropdownMenuItem>
+            <Pencil />
+            <span>Edit Name</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Share />
+            <span>Share</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Copy />
+            <span>Copy</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Eye />
+            <span>Make Public</span>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={handleDelete}>
+            <Trash />
+            <span>Delete</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
 
     </Button>
   )
