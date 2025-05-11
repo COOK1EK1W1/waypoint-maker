b6f3e94d05ed4eddf0bafad70f55b325c59090be
Panda-Aadham <145044546+Panda-Aadham@users.noreply.github.com>
2025-05-11 15:21:58 +0100

    Added loading spinner when creating a new mission

--- src/components/dashboard/createMission.tsx
+++ src/components/dashboard/createMission.tsx
@@ -2,10 +2,13 @@
 
 import { useRouter } from "next/navigation"
 import Button from "../toolBar/button"
+import { Loader2 } from "lucide-react"
+import { useTransition } from "react"
 import { newProj } from "./actions"
 
 export default function CreateMission() {
   const router = useRouter()
+  const [isPending, startTransition] = useTransition()
 
   function handleCreate() {
     const title = prompt("Enter Mission Name")
@@ -13,17 +16,25 @@ export default function CreateMission() {
       alert("Please use a valid name")
       return
     }
-    newProj(title).then((res) => {
-      if (res.error !== null) {
-        alert(res.error)
-        return
-      }
-      router.push("/m/" + res.data.id)
-    })
 
+    startTransition(() => {
+      newProj(title).then((res) => {
+        if (res.error !== null) {
+          alert(res.error)
+          return
+        }
+        router.push("/m/" + res.data.id)
+      })
+    })
   }
   return (
-    <Button onClick={handleCreate} className="bg-white w-full justify-center p-2 my-3">Create New Mission</Button>
+    <Button onClick={handleCreate} className="bg-white w-full justify-center p-2 my-3" disabled={isPending}>
+      {isPending ? (
+        <Loader2 className="animate-spin h-5 w-5 text-gray-700" />
+      ) : (
+        "Create New Mission"
+      )}
+    </Button>
   )
 
 }
