7ef4463e625eb368d2c95d35c7a8b9b61d769445
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2025-05-13 22:52:43 +0000

    add privacy and tos pages

--- src/components/toolBar/user.tsx
+++ src/components/toolBar/user.tsx
@@ -7,6 +7,7 @@ import LoginModal from "../modal/login";
 import { DialogDescription } from "@radix-ui/react-dialog";
 import { auth } from "@/util/auth";
 import { headers } from "next/headers";
+import Image from "next/image";
 
 export function UserSkeleton() {
   return <Button className="w-28"><span className="h-6 rounded-full animate-pulse w-20 bg-slate-300" /></Button>
@@ -18,7 +19,7 @@ export default async function User() {
   return (< Dialog >
     <DialogTrigger asChild>
       {data?.user ?
-        <Button className="w-28 justify-start"><img height={20} width={20} className="rounded-full" alt="profile picture" src={data.user.image || ""} /><span className="grow">Account</span></Button> :
+        <Button className="w-28 justify-start"><Image height={20} width={20} className="rounded-full" alt="profile picture" src={data.user.image || ""} /><span className="grow">Account</span></Button> :
         <Button className="w-28">Sign In</Button>
       }
     </DialogTrigger>
@@ -31,7 +32,7 @@ export default async function User() {
             <div className="flex items-center gap-2">
               Logged in as:
               <span className="flex items-center gap-2 bg-gray-100 px-2 py-1 rounded-md shadow-sm max-w-[210px]">
-                <img height={20} width={20} className="rounded-full" alt="profile picture" src={data.user.image || ""} />
+                <Image height={20} width={20} className="rounded-full" alt="profile picture" src={data.user.image || ""} />
                 <span className="truncate whitespace-nowrap overflow-hidden">{data.user.name}</span>
               </span>
             </div>
