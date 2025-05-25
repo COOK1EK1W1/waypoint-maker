38d58c2dadb240806d158b9082b4b43c5d1ad9ac
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2025-05-25 11:47:54 +0100

    fix dropdown issue

--- src/components/waypointList/ListItem.tsx
+++ src/components/waypointList/ListItem.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { Button } from "../ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "../ui/dropdown-menu";
 import { EllipsisVertical } from "lucide-react";
@@ -12,6 +12,8 @@ export default function ListItem({ icon, name, onClick, className, selected, men
   selected?: boolean,
   menuItems?: ReactNode
 }) {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
   return (
     <div className="px-2 py-1">
       <Button variant={selected ? "active" : "default"} className={cn("w-full p-2 m-0 h-auto flex", className)
@@ -19,15 +21,15 @@ export default function ListItem({ icon, name, onClick, className, selected, men
         <div className="flex-grow text-start flex items-center" onClick={onClick}>
           <span className="inline-block mr-1">{icon}</span><span className="inline-block">{name}</span>
         </div>
-        <DropdownMenu>
+        <DropdownMenu open={dropdownOpen} onOpenChange={setDropdownOpen}>
 
           <DropdownMenuTrigger asChild>
-            <div className="p-1 hover:bg-slate-100 rounded-full transition-colors" >
+            <div className="p-1 hover:bg-slate-100 rounded-full transition-colors" onClick={() => setDropdownOpen(true)}>
               <EllipsisVertical className="h-5 w-5 text-slate-500" />
             </div>
           </DropdownMenuTrigger>
 
-          <DropdownMenuContent align="end" className="w-48">
+          <DropdownMenuContent align="end" className="w-48" onClick={() => setDropdownOpen(false)}>
             {menuItems}
           </DropdownMenuContent>
         </DropdownMenu>
